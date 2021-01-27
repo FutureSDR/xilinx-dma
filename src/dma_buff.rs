7c8fbd4ab2473a1545639e976e3899610eed6ffb
Bastian Bloessl <mail@bastibl.net>
2021-01-27 07:26:36 +0100

    init

---
+++ src/dma_buff.rs
@@ -0,0 +1,108 @@
+use anyhow::Result;
+use std::io::prelude::*;
+use std::fmt;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::os::unix::io::AsRawFd;
+
+pub struct DmaBuff {
+    name: String,
+    size: usize,
+    phys_addr: usize,
+    buffer: *mut libc::c_void,
+    sync_mode: bool,
+    debug_vma: bool,
+}
+
+impl fmt::Debug for DmaBuff {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("DmaBuff")
+         .field("name", &self.name)
+         .field("size", &self.size)
+         .finish()
+    }
+}
+
+impl DmaBuff {
+    pub fn new(name: &str) -> Result<DmaBuff> {
+
+        let phy_f = format!("/sys/class/u-dma-buff/{}/phys_addr", name);
+        let mut phy_f = File::open(phy_f)?;
+        let mut buff = String::new();
+        phy_f.read_to_string(&mut buff)?;
+        let buff = buff.trim().trim_start_matches("0x");
+        let phys_addr = usize::from_str_radix(buff, 16)?;
+
+        let size_f = format!("/sys/class/u-dma-buff/{}/size", name);
+        let mut size_f = File::open(size_f)?;
+        let mut buff = String::new();
+        size_f.read_to_string(&mut buff)?;
+        let buff = buff.trim();
+        let size = buff.parse::<usize>()?;
+
+        let debug_f = format!("/sys/class/u-dma-buff/{}/debug_vma", name);
+        let mut debug_f = File::open(debug_f)?;
+        let mut buff = String::new();
+        debug_f.read_to_string(&mut buff)?;
+        let debug_vma = if buff.trim() == "0" {
+            false
+        } else {
+            true
+        };
+
+        let sync_f = format!("/sys/class/u-dma-buff/{}/sync_mode", name);
+        let mut sync_f = File::open(sync_f)?;
+        let mut buff = String::new();
+        sync_f.read_to_string(&mut buff)?;
+        let sync_mode = if buff.trim() == "0" {
+            false
+        } else {
+            true
+        };
+
+        let dev = format!("/dev/{}", name);
+        let dev = OpenOptions::new().read(true).write(true).open(dev)?;
+
+        let buffer;
+        unsafe {
+            buffer = libc::mmap(0 as *mut libc::c_void, size, libc::PROT_READ|libc::PROT_WRITE, libc::MAP_SHARED, dev.as_raw_fd(), 0);
+            if buffer == libc::MAP_FAILED {
+                panic!("mapping dma bufffer into virtual memory failed");
+            }
+        }
+
+        Ok(DmaBuff {
+            name: name.to_string(),
+            size,
+            phys_addr,
+            buffer,
+            sync_mode,
+            debug_vma,
+        })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn phys_addr(&self) -> usize {
+        self.phys_addr
+    }
+
+    pub fn buffer(&self) -> *mut libc::c_void {
+        self.buffer
+    }
+
+    pub fn sync_mode(&self) -> bool {
+        self.sync_mode
+    }
+
+    pub fn debug_vma(&self) -> bool {
+        self.debug_vma
+    }
+}
+
