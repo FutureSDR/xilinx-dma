dcdb358a7c7a6246dc3020ca97429e1f5705923e
Daniel Estévez <daniel@destevez.net>
2023-07-17 09:49:20 +0200

    put all async code behind a feature flag
    This adds a feature flag 'async', which is enabled by default, to
    control whether async code is built. This allows projects that do
    not use the async implementation to avoid depending on async-io.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 mod axi_dma;
+
+#[cfg(feature = "async")]
 mod axi_dma_async;
+
 mod dma_buffer;
 pub use axi_dma::AxiDma;
+
+#[cfg(feature = "async")]
 pub use axi_dma_async::AxiDmaAsync;
+
 pub use dma_buffer::DmaBuffer;
 
 #[cfg(xilinx_dma_has_dmb)]
